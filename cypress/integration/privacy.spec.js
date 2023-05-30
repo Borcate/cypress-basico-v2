7135abe5a4ecbd94fcbdb0980655f652d15d6bc8
Janaina Borcate <borcatejana@gmail.com>
2023-05-30 17:21:31 -0300

    alteracao privacy.spec.js

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,6 @@
 //Exercício extra 2 - Desafio
 it('testa a página da política de privacidade de forma independente', () => {
     cy.visit('./src/privacy.html')
-    cy.contains('Talking About logo').should('be.visible')
+    cy.contains('Talking About Testing').should('be.visible')
     
 });
